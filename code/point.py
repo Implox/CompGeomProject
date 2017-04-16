eb8e94702f33421d0ed82b9a6a74b64ac48836c8
Implox <alexmflores@gmail.com>
2017-04-16 12:40:11 -0400

    Adds delauny algorithm, modifies KineticPoint2d

--- code/point.py
+++ code/point.py
@@ -21,3 +21,17 @@ class Point2d(object):
     @staticmethod
     def midpoint(a, b):
         return Point2d(0.5 * (a.x + b.x), 0.5 * (a.y + b.y))
+
+class KineticPoint2d(Point2d):
+    def __init__(self,x,y,dx,dy):
+        super().__init__(x,y)
+        self.dx = dx
+        self.dy = dy
+
+    def __str__(self):
+        return('pos({},{}) | vel({},{})'.format(self.x,self.y,self.vx,self.vy))
+
+    def position_at(self, t):
+        x_prime = self.x + t * self.dx
+        y_prime = self.y + t * self.dy
+        return Point2d(x_prime, y_prime)
